7a1639a6df8582a4638cbbbc4e17c0768ed6e113
cdangg <dang.celia@gmail.com>
2014-03-04 18:37:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roomami::Application.config.secret_key_base = '71bb86553661846f83b5e3d786367d9c40720c5fb7d613872bbfa46ddc3a11e95629e6908299be2a88b926f18e7a1eff4ec1d88e03b10fe1bda187fcab3fd8b8'

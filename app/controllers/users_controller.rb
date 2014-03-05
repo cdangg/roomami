087cd120062867cdf2e48ed353d54a162fbc8f6f
andrewjbotros <andrewjbotros@gmail.com>
2014-03-05 13:06:00 -0500

    update view page for my profile details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
   	end
   end
 
+  def update
+
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   private
 

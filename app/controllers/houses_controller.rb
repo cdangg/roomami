2bd0017e63f1ce3f4512d9689d1dca6457378e7d
cdangg <dang.celia@gmail.com>
2014-03-05 16:39:34 -0500

    Fixed house edit link on index page

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,12 +5,12 @@ class HousesController < ApplicationController
     @houses = House.all
   end
 
-  def new
-    @house = House.new
-  end
-
   def edit
     @house = House.find(params[:id])
+  end
+
+  def new
+    @house = House.new
   end
 
   def create

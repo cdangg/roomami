e4dac6f26c13bb215bb7d14a116ef066a7860b81
andrewjbotros <andrewjbotros@gmail.com>
2014-03-05 21:46:44 -0500

    add ensured login to applicable controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
 	def create
 	  user = login(params[:email], params[:password])
 	  if user
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	before_filter :require_login, :only => :secret
-	before_filter :ensure_logged_in
+	before_filter :ensure_logged_in, :except => :index
 
 	def index
 	end

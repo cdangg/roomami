fee584844bb3708045efd42b88e434822bc02cee
cdangg <dang.celia@gmail.com>
2014-03-13 23:45:59 -0400

    show defaut avatar on houses/show view

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -25,7 +25,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [100, 100]
+  process :scale => [200, 200]
 
   # def scale(width, height)
   #   # do something
@@ -33,7 +33,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit=> [100, 100]
   end
 
   version :thumby do
